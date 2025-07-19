2cc693d825baf3371ab1d89ca8f3aee9eba5449c
HoneyHanny <hansduran25@gmail.com>
2025-07-19 23:58:12 +0800

    Update packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,12 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  plugins: [viteReact(), tailwindcss()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': '/src',
     },
   },
 })
